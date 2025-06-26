052391ec9313511b36406c4c9a754fb8b54aa57b
amrouehab <amrou.ehab@gmail.com>
2025-06-26 19:16:11 +0000

    PrimeGame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
 
-def is_prime(n):
-    """Check if number is a prime"""
-    if n < 2:
-        return False
+def sieve(n):
+    """Returns a list of the number of primes up to index i for i in range(n + 1)."""
+    is_prime = [False, False] + [True] * (n - 1)
     for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def prime_count(n):
-    """Return number of prime numbers <= n using sieve"""
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
+        if is_prime[i]:
+            for j in range(i*i, n+1, i):
+                is_prime[j] = False
+    prime_count = [0] * (n + 1)
     count = 0
-    for i in range(2, n + 1):
-        if sieve[i]:
+    for i in range(n + 1):
+        if is_prime[i]:
             count += 1
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    return count
+        prime_count[i] = count
+    return prime_count
 
 def isWinner(x, nums):
-    """Determine the winner of the prime game"""
+    """Determines the winner of the prime game."""
     if not nums or x < 1:
         return None
+    max_num = max(nums)
+    prime_counts = sieve(max_num)
 
     maria_wins = 0
     ben_wins = 0
 
-    for i in range(x):
-        primes = prime_count(nums[i])
-        if primes % 2 == 0:
-            ben_wins += 1
-        else:
+    for n in nums:
+        if prime_counts[n] % 2 == 1:
             maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
-    return None
+    else:
+        return None
+
